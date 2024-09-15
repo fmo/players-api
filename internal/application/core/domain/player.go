e79d758586bccce73f3a2f04b57942708430564c
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-09-15 12:59:53 +0200

    add the all values

--- internal/application/core/domain/player.go
+++ internal/application/core/domain/player.go
@@ -1,10 +1,12 @@
 package domain
 
 type Player struct {
-	Age         *int    `json:"age,omitempty"`
-	Id          *string `json:"id,omitempty"`
-	Name        *string `json:"name,omitempty"`
-	Position    *string `json:"position,omitempty"`
-	ShirtNumber *string `json:"shirtNumber,omitempty"`
-	TeamId      *int    `json:"teamId,omitempty"`
+	Age                 *int    `json:"age,omitempty"`
+	Id                  *string `json:"id,omitempty"`
+	Name                *string `json:"name,omitempty"`
+	Position            *string `json:"position,omitempty"`
+	ShirtNumber         *string `json:"shirtNumber,omitempty"`
+	TeamId              *int    `json:"teamId,omitempty"`
+	MarketValue         int     `json:"marketValue"`
+	MarketValueCurrency string  `json:"marketValueCurrency"`
 }
